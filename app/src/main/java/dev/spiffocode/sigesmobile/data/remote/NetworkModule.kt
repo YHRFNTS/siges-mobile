dd9fcb59c671d9abc28880073055a448ebf7474c
Caryuter <111262772+Caryuter@users.noreply.github.com.>
2026-03-19 19:44:16 -0600

    feat: add equipment api service

--- app/src/main/java/dev/spiffocode/sigesmobile/data/remote/NetworkModule.kt
+++ app/src/main/java/dev/spiffocode/sigesmobile/data/remote/NetworkModule.kt
@@ -5,14 +5,19 @@ import com.google.gson.GsonBuilder
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
-import dev.spiffocode.sigesmobile.data.remote.AuthInterceptor
-import dev.spiffocode.sigesmobile.data.remote.TokenAuthenticator
-import dev.spiffocode.sigesmobile.data.remote.api.AuthApiService
-import dev.spiffocode.sigesmobile.data.remote.api.ReservationApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dev.spiffocode.sigesmobile.data.remote.api.AuthApiService
+import dev.spiffocode.sigesmobile.data.remote.api.BuildingApiService
+import dev.spiffocode.sigesmobile.data.remote.api.EquipmentApiService
+import dev.spiffocode.sigesmobile.data.remote.api.NotificationApiService
+import dev.spiffocode.sigesmobile.data.remote.api.PasswordRecoveryApiService
+import dev.spiffocode.sigesmobile.data.remote.api.ReportApiService
+import dev.spiffocode.sigesmobile.data.remote.api.ReservationApiService
+import dev.spiffocode.sigesmobile.data.remote.api.SpaceApiService
+import dev.spiffocode.sigesmobile.data.remote.api.UserApiService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -76,13 +81,39 @@ object NetworkModule {
         .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
 
-    @Provides
-    @Singleton
+    @Provides @Singleton
     fun provideAuthApiService(retrofit: Retrofit): AuthApiService =
         retrofit.create(AuthApiService::class.java)
 
-    @Provides
-    @Singleton
+    @Provides @Singleton
+    fun provideUserApiService(retrofit: Retrofit): UserApiService =
+        retrofit.create(UserApiService::class.java)
+
+    @Provides @Singleton
+    fun provideBuildingApiService(retrofit: Retrofit): BuildingApiService =
+        retrofit.create(BuildingApiService::class.java)
+
+    @Provides @Singleton
+    fun provideSpaceApiService(retrofit: Retrofit): SpaceApiService =
+        retrofit.create(SpaceApiService::class.java)
+
+    @Provides @Singleton
+    fun provideEquipmentApiService(retrofit: Retrofit): EquipmentApiService =
+        retrofit.create(EquipmentApiService::class.java)
+
+    @Provides @Singleton
     fun provideReservationApiService(retrofit: Retrofit): ReservationApiService =
         retrofit.create(ReservationApiService::class.java)
+
+    @Provides @Singleton
+    fun provideNotificationApiService(retrofit: Retrofit): NotificationApiService =
+        retrofit.create(NotificationApiService::class.java)
+
+    @Provides @Singleton
+    fun providePasswordRecoveryApiService(retrofit: Retrofit): PasswordRecoveryApiService =
+        retrofit.create(PasswordRecoveryApiService::class.java)
+
+    @Provides @Singleton
+    fun provideReportApiService(retrofit: Retrofit): ReportApiService =
+        retrofit.create(ReportApiService::class.java)
 }
---
+++ app/src/main/java/dev/spiffocode/sigesmobile/data/remote/api/EquipmentApiService.kt
@@ -0,0 +1,48 @@
+package dev.spiffocode.sigesmobile.data.remote.api
+
+import dev.spiffocode.sigesmobile.data.remote.dto.EquipmentDto
+import dev.spiffocode.sigesmobile.data.remote.dto.EquipmentTypeDto
+import dev.spiffocode.sigesmobile.data.remote.dto.PageEquipmentDto
+import dev.spiffocode.sigesmobile.data.remote.dto.ReservableStatus
+import dev.spiffocode.sigesmobile.data.remote.dto.ShowMode
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import java.time.LocalDate
+
+interface EquipmentApiService {
+
+    @GET("equipments")
+    suspend fun searchEquipments(
+        @Query("page")              page: Int = 0,
+        @Query("size")              size: Int = 20,
+        @Query("sort")              sort: String?,
+        @Query("searchQuery")       searchQuery: String?,
+        @Query("status")            status: ReservableStatus?,
+        @Query("buildingId")        buildingId: Long?,
+        @Query("studentsAvailable") studentsAvailable: Boolean?,
+        @Query("spaceId")           spaceId: Long?,
+        @Query("equipmentTypeId")   equipmentTypeId: Long?,
+        @Query("requestStart")      requestStart: LocalDate?,
+        @Query("requestEnd")        requestEnd: LocalDate?,
+        @Query("showMode")          showMode: ShowMode?
+    ): Response<PageEquipmentDto>
+
+    @GET("equipments/{id}")
+    suspend fun getEquipment(
+        @Path("id") id: Long
+    ): Response<EquipmentDto>
+
+
+    @GET("equipment-types")
+    suspend fun getAllEquipmentTypes(
+        @Query("q")        q: String? = null,
+        @Query("showMode") showMode: ShowMode? = null
+    ): Response<List<EquipmentTypeDto>>
+
+    @GET("equipment-types/{id}")
+    suspend fun getEquipmentType(
+        @Path("id") id: Long
+    ): Response<EquipmentTypeDto>
+}
